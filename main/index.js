b6b12786010206f2ed590d74d865779c1221a942
illonion <zhou.lawrence.yz@gmail.com>
2024-07-08 15:09:40 +1000

    Add create star logic

--- main/index.js
+++ main/index.js
@@ -10,6 +10,11 @@ const purpleTeamNameEl = document.getElementById("purpleTeamName")
 const blueTeamNameEl = document.getElementById("blueTeamName")
 let currentPurpleTeamName, currentBlueTeamName
 
+// Stars
+const purpleTeamStarsEl = document.getElementById("purpleTeamStars")
+const blueTeamStarsEl = document.getElementById("blueTeamStars")
+let currentBestOf, currentFirstTo, currentPurpleTeamStars, currentBlueTeamStars
+
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
@@ -22,5 +27,36 @@ socket.onmessage = event => {
     if (currentBlueTeamName !== data.tourney.manager.teamName.right) {
         currentBlueTeamName = data.tourney.manager.teamName.right
         blueTeamNameEl.innerText = currentBlueTeamName
+    }
+
+    // Stars
+    if (currentBestOf !== data.tourney.manager.bestOF ||
+        currentPurpleTeamStars !== data.tourney.manager.stars.left ||
+        currentBlueTeamStars !== data.tourney.manager.stars.right
+    ) {
+        // Set current information
+        currentBestOf = data.tourney.manager.bestOF
+        currentFirstTo = Math.ceil(currentBestOf / 2)
+        currentPurpleTeamStars = data.tourney.manager.stars.left
+        currentBlueTeamStars = data.tourney.manager.stars.right
+
+        // Reset stars
+        purpleTeamStarsEl.innerHTML = ""
+        blueTeamStarsEl.innerHTML = ""
+
+        // Create star
+        function createStar(starFill) {
+            const newStar = document.createElement("div")
+            newStar.classList.add("teamStar")
+            if (starFill) newStar.classList.add("teamStarFill")
+            return newStar
+        }
+
+        let i = 0
+        for (i; i < currentPurpleTeamStars; i++) purpleTeamStarsEl.append(createStar(true))
+        for (i; i < currentFirstTo; i++) purpleTeamStarsEl.append(createStar(false))
+        i = 0
+        for (i; i < currentBlueTeamStars; i++) blueTeamStarsEl.append(createStar(true))
+        for (i; i < currentFirstTo; i++) blueTeamStarsEl.append(createStar(false))
     }
 }
