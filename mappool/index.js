c4b5bd46cac279cadc0a172c3275775f3015f268
illonion <zhou.lawrence.yz@gmail.com>
2024-08-24 02:07:07 +1000

    Add map pick events, automatic picking, automatic winner setting

--- mappool/index.js
+++ mappool/index.js
@@ -7,6 +7,7 @@ let numberOfPicks = 12
 
 const purpleTeamPickCardsEl = document.getElementById("purpleTeamPickCards")
 const blueTeamPickCardsEl = document.getElementById("blueTeamPickCards")
+const allMapsButtonContainerEl = document.getElementById("allMapsButtonContainer")
 
 async function getMappool() {
     const response = await fetch("http://127.0.0.1:24050/SRS/_data/beatmaps.json")
@@ -34,16 +35,29 @@ async function getMappool() {
 
         const pickCardWinnerContainer = document.createElement("div")
         pickCardWinnerContainer.classList.add("pickCardWinnerContainer")
-        const pickCardWinnerImage = document.createElement("div")
+        const pickCardWinnerImage = document.createElement("img")
         pickCardWinnerImage.classList.add("pickCardWinnerImage")
         pickCardWinnerContainer.append(pickCardWinnerImage)
 
         const pickCardMod = document.createElement("div")
         pickCardMod.classList.add("pickCardMod")
+        if (i % 2 === 0) pickCardMod.classList.add("pickCardModPurple")
+        else pickCardMod.classList.add("pickCardModBlue")
 
         pickCard.append(pickCardBackgroundImage, pickCardImageContainer, pickCardWinnerContainer, pickCardMod)
         if (i % 2 === 0) purpleTeamPickCardsEl.append(pickCard)
         else blueTeamPickCardsEl.append(pickCard)
+    }
+
+    // Generate buttons
+    for (let i = 0 ; i < allBeatmaps.length - 1; i++) {
+        const mapsButton = document.createElement("button")
+        mapsButton.classList.add("mapsButton")
+        mapsButton.innerText = `${allBeatmaps[i].mod.toUpperCase()}${allBeatmaps[i].order}`
+        mapsButton.dataset.id = allBeatmaps[i].beatmapID
+        mapsButton.setAttribute("id", allBeatmaps[i].beatmapID)
+        mapsButton.addEventListener("click", mapClickEvent)
+        allMapsButtonContainerEl.append(mapsButton)
     }
 }
 getMappool()
@@ -74,6 +88,13 @@ let starVisible
 // Chat Display
 const chatDisplayEl = document.getElementById("chatDisplay")
 let chatLength = 0
+
+// Beatmap information
+let currentMapId, currentMapMd5
+
+// Set winner
+let ipcState
+let setWinnerAlready = false
 
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
@@ -174,4 +195,131 @@ socket.onmessage = event => {
         chatLen = data.tourney.manager.chat.length;
         chatDisplayEl.scrollTop = chatDisplayEl.scrollHeight;
     }
+
+    // Beatmap stuff
+    if (currentMapId !== data.menu.bm.id || currentMapMd5 !== data.menu.bm.md5) {
+        currentMapId = data.menu.bm.id
+        currentMapMd5 = data.menu.bm.md5
+
+        const currentMap = findMapInMappool(currentMapId)
+        if (currentMap && currentAutopick && currentAction === "pick") {
+            document.getElementById(currentMapId).click()
+        }
+    }
+
+    // IPC State
+    if (ipcState !== data.tourney.manager.ipcState) {
+        ipcState = data.tourney.manager.ipcState
+        if (ipcState === 4) {
+            if (!setWinnerAlready) {
+                setWinnerAlready = true
+                let currentRedScore = 0
+                let currentBlueScore = 0
+                
+                for (let i = 0; i < data.tourney.ipcClients.length; i++) {
+                    let currentScore = 0
+                    currentScore = data.tourney.ipcClients[i].gameplay.score * (data.tourney.ipcClients[i].gameplay.mods.str.includes("EZ")? 1.75 : 1)
+                    if (data.tourney.ipcClients[i].team === "left") currentRedScore += currentScore
+                    else currentBlueScore += currentScore
+                }
+
+                if (currentRedScore > currentBlueScore) {
+                    currentPickTile.children[2].classList.add("pickCardWinnerPurple")
+                    currentPickTile.children[2].children[0].setAttribute("src", "static/popsicle-purple.png")
+                } else if (currentRedScore < currentBlueScore) {
+                    currentPickTile.children[2].classList.add("pickCardWinnerBlue")
+                    currentPickTile.children[2].children[0].setAttribute("src", "static/popsicle-blue.png")
+                }
+            }
+        } else {
+            setWinnerAlready = false
+        }
+    }
+}
+
+// Set next picker
+let currentActionTeam = 'red'
+let currentAction = 'pick'
+const nextPickerTextEl = document.getElementById("nextPickerText")
+function setNextAction(colour, action) {
+    currentActionTeam = colour
+    currentAction = action
+    nextPickerTextEl.innerText = `${colour} ${action}`
+}
+
+// Toggle autopick
+let currentAutopick = false
+const toggleAutopickTextEl = document.getElementById("toggleAutopickText")
+function toggleAutopick() {
+    currentAutopick = !currentAutopick
+    if (currentAutopick) toggleAutopickTextEl.innerText = "ON"
+    else toggleAutopickTextEl.innerText = "OFF"
+}
+
+// Map click event
+// ban cards
+const purpleTeamBanCardsEl = document.getElementById("purpleTeamBanCards")
+const blueTeamBanCardsEl = document.getElementById("blueTeamBanCards")
+let currentPickTile
+function mapClickEvent() {
+    // Get current map
+    let currentMapId = this.dataset.id
+    let currentMap = findMapInMappool(parseInt(currentMapId))
+
+    // Check if map has been banned or picked before
+    const allIdElements = document.querySelectorAll("[data-id]")
+    let mapFound = false
+    allIdElements.forEach(element => {
+        if (element.dataset.id == currentMapId && (element.dataset.action === "ban" || element.dataset.action === "pick")) {
+            mapFound = true
+            return
+        }   
+    })
+    if (mapFound) return
+
+    // Set a ban
+    if (currentAction === "ban") {
+        // Set current tile that we will be banning
+        let currentCardContainer
+        currentCardContainer = (currentActionTeam === "red")? purpleTeamBanCardsEl : blueTeamBanCardsEl
+
+        // Check if first tile has something
+        let currentTile
+        if (currentCardContainer.children[0].hasAttribute("data-id")) currentTile = currentCardContainer.children[1]
+        else currentTile = currentCardContainer.children[0]
+
+        // Set details for the ban
+        currentTile.children[1].style.backgroundImage = `url(${currentMap.imgURL})`
+        currentTile.dataset.id = this.dataset.id
+        currentTile.dataset.action = "ban"
+    }
+
+    // Set a pick
+    if (currentAction === "pick") {
+        // set current tiel that we will be piking
+        let currentCardContainer
+        currentCardContainer = (currentActionTeam === "red")? purpleTeamPickCardsEl : blueTeamPickCardsEl
+
+        // Check which tile has something
+        let currentTile
+        for (let i = 0; i < currentCardContainer.childElementCount; i++) {
+            if (currentCardContainer.children[i].hasAttribute("data-id")) continue
+            currentTile = currentCardContainer.children[i]
+            break
+        }
+        if (!currentTile) return
+
+        // Set details for the pick
+        currentTile.children[1].children[0].style.backgroundImage = `url(${currentMap.imgURL})`
+        currentTile.children[3].innerText = `${currentMap.mod.toUpperCase()}${currentMap.order}`
+        currentTile.children[3].style.opacity = 1
+        currentTile.dataset.id = this.dataset.id
+        currentTile.dataset.action = "pick"
+        currentPickTile = currentTile
+
+        setNextAction
+    }
+
+    if (currentActionTeam === "red") setNextAction("blue", currentAction)
+    else setNextAction("red", currentAction)
 }
